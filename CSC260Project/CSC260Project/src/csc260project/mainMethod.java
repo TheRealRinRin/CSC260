d64cf2f245193d1dcdf511ee6358f02c77db8933
bosspml <102826076+bosspml@users.noreply.github.com>
2025-11-25 22:26:19 +0000

    main method created

---
+++ CSC260Project/CSC260Project/src/csc260project/mainMethod.java
@@ -0,0 +1,9 @@
+package csc260project;
+
+public class mainMethod {
+    public static void main(String[] args) {
+        //ReadCSV.main(args);
+        // main app where all the methods are called
+    }
+    
+}
