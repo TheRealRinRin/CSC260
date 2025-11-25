11809d8717bf67a8a880b66f6234b08c775f0a80
bosspml <102826076+bosspml@users.noreply.github.com>
2025-11-25 22:22:11 +0000

    Stack plan

--- CSC260Project/CSC260Project/src/csc260project/StackUndo.java
+++ CSC260Project/CSC260Project/src/csc260project/StackUndo.java
@@ -2,4 +2,8 @@ package csc260project;
 
 public class StackUndo {
     // stack to implement undo functionality
+    // have an undo method in main that run stack pop
+    // Stack will store diferent states of the linked list
+    // to run linked list, must go through stack to get to current state
+    // when undo is called, pop the stack to get previous state
 }
